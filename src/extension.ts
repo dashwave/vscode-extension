c76d1d418dc15e9fd06605d201f4b617cd6519aa
Supratik Das <rick.das08@gmail.com>
2024-05-27 14:25:18 +0530

    Re-enable tree view in sidebar

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { installDW } from './pluginStartup';
+import { OutputConsole } from './components/outputConsole';
 const { exec } = require('child_process');
 
 
@@ -12,9 +13,37 @@ export function activate(context: vscode.ExtensionContext) {
         // Replace 'your-cli-command' with the actual CLI command you want to run
         // Prepare your CLI command
 		const cliCommand = 'whoami';
+
+    const dwOutput = new OutputConsole();
+
+    installDW(null, dwOutput); // Include the dwOutput argument
     });
 
 	context.subscriptions.push(disposable);
+
+  const dashwaveActionsProvider = new DashwaveActionsProvider();
+  vscode.window.registerTreeDataProvider('dashwaveView', dashwaveActionsProvider);
+}
+
+class DashwaveActionsProvider {
+  getTreeItem(element: any) {
+      return element;
+  }
+
+  getChildren(element: any) {
+      if (element === undefined) {
+          return ['Build', 'Emulate', 'Debug'].map(action => {
+              const treeItem = new vscode.TreeItem(action);
+                  treeItem.command = { 
+                      command: 'extension.runCliCommand', 
+                      title: 'Run CLI Command', 
+                      arguments: [action] // Pass the fruit name as an argument
+                  };
+              return treeItem;
+          });
+      }
+      return [];
+  }
 }
 
 // This method is called when your extension is deactivated
